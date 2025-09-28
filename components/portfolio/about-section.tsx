9dea9d7c4b9863a2e3b1f65ae83c00321ff58afd
Jovaniew <jovaniewijaya23@gmail.com>
2025-09-29 00:32:03 +0700

    Update about-section.tsx

--- components/portfolio/about-section.tsx
+++ components/portfolio/about-section.tsx
@@ -7,7 +7,7 @@ export function AboutSection() {
         <p className="text-pretty">
           Hi! I’m a Computer Science student specializing in UI/UX Design, passionate about turning logic into intuitive 
           and meaningful digital experiences. I love exploring how people interact with technology — combining research, 
-          visual storytelling, and communication skills to design interfaces that not only work, but feel right.
+          visual storytelling, and communication skills.
         </p>
         <p className="mt-3 text-pretty">
           Outside of design, I enjoy learning foreign languages,
